fd055c11d3e76ba399363d8194c4180b2e855682
gityoav <yoav.git@gmail.com>
2021-12-07 01:33:56 +0000

    remove version dependency

--- src/setup.py
+++ src/setup.py
@@ -4,4 +4,4 @@ import platform
 from setuptools import setup, find_packages
 from distutils.core import Extension
 
-setup(name = 'pyg-base', version = '0.0.1', packages = find_packages(), python_requires = '>=3.7.')
+setup(name = 'pyg-base', version = '0.0.2', packages = find_packages(), python_requires = '>=3.7.')
