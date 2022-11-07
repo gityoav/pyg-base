85b8d3aa05a850dd7302752b1c94c8dec5923c46
Yoav Git <yoav.git@gmail.com>
2022-11-07 17:42:10 +0000

    v40

--- src/setup.py
+++ src/setup.py
@@ -4,4 +4,4 @@ import platform
 from setuptools import setup, find_packages
 from distutils.core import Extension
 
-setup(name = 'pyg-base', version = '0.0.39', packages = find_packages(), python_requires = '>=3.6.')
+setup(name = 'pyg-base', version = '0.0.40', packages = find_packages(), python_requires = '>=3.6.')
