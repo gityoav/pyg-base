d8a4b8d225bb76a6c46c4a5b94e361588d41df18
Yoav Git <yg@greshamllc.com>
2023-11-03 12:12:46 +0000

    dding support for percent and bp

--- src/pyg_base/_as_float.py
+++ src/pyg_base/_as_float.py
@@ -1,7 +1,7 @@
 from pyg_base._loop import loop
 from pyg_base._types import is_str, is_num
 
-_endings = [('mln', 6), ('mn', 6), ('m', 6), ('bln', 9), ('bn', 9), ('b', 9), ('tln', 12), ('tn', 12), ('t', 12), ('%', -2), ('k', 3)]
+_endings = [('million',6), ('billion', 9), ('trillion', 12), ('percent', -2), ('mln', 6), ('bln', 9), ('tln', 12), ('pct', -2), ('mn', 6), ('bn', 9), ('tn', 12), ('bp', -4), ('%', -2), ('m', 6), ('k', 3), ('b', 9), ('t', 12)]
 _n = [(k, 10**v) for k, v in _endings]
 
 __all__ = ['as_float']
@@ -35,10 +35,12 @@ def _as_float(value):
     >>> from pyg import *
     >>> assert as_float('1.3k') == 1300
     >>> assert as_float('1.4m') == 1400000
-    >>> assert as_float('1.4mln') == 1400000
+    >>> assert as_float('1.4 mln') == 1400000
     >>> assert as_float('1.4bn') == 1400000000
     >>> assert as_float('1.4tln') == 1400000000000
     >>> assert as_float('100%') == 1
+    >>> assert as_float('100 pct') == 1
+    >>> assert as_float('100bp') == 0.01
     >>> assert as_float('1,234') == 1234
     >>> assert as_float('-1,234k') == -1234000
     """
