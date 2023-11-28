fa580b2244ec298ceadd5b3e288aa49ef81604ee
Yoav Git <yoav.git@gmail.com>
2023-11-28 16:53:44 +0000

    support as_float for bln

--- src/pyg_base/_as_float.py
+++ src/pyg_base/_as_float.py
@@ -1,7 +1,7 @@
 from pyg_base._loop import loop
 from pyg_base._types import is_str, is_num
 
-_endings = [('million',6), ('billion', 9), ('trillion', 12), ('percent', -2), ('mln', 6), ('bln', 9), ('tln', 12), ('pct', -2), ('mn', 6), ('bn', 9), ('tn', 12), ('bp', -4), ('%', -2), ('m', 6), ('k', 3), ('b', 9), ('t', 12)]
+_endings = [('million',6), ('billion', 9), ('trillion', 12), ('percent', -2), ('mln', 6), ('bln', 9), ('tln', 12), ('trl', 12), ('pct', -2), ('mn', 6), ('bn', 9), ('tn', 12), ('bp', -4), ('%', -2), ('m', 6), ('k', 3), ('b', 9), ('t', 12), ('crore', 7), ('lakh', 5)]
 _n = [(k, 10**v) for k, v in _endings]
 
 __all__ = ['as_float']
@@ -10,6 +10,8 @@ def _txt_and_mult(txt):
     """
     """
     lower_txt = txt.lower()
+    if len(txt) < 2 or txt[-1].isdigit():
+        return txt, 1
     for k, mult in _n:
         if lower_txt.endswith(k):
             return txt[:-len(k)], mult
@@ -43,6 +45,8 @@ def _as_float(value):
     >>> assert as_float('100bp') == 0.01
     >>> assert as_float('1,234') == 1234
     >>> assert as_float('-1,234k') == -1234000
+    >>> assert as_float('1.2 lakh') == 120000 ## indian rupees notations
+    >>> assert as_float('1.2 crore') == 12000000 ## indian rupees notations
     """
     if is_str(value):
         txt = value.replace(',','').replace(' ','')
