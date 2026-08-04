97fde8d4d2cb441913f638a288e80f83dcae1b6a
Yoav Git <30290427+gityoav@users.noreply.github.com>
2026-08-04 09:51:51 +0100

    v113

--- src/setup.py
+++ src/setup.py
@@ -4,4 +4,4 @@ import platform
 from setuptools import setup, find_packages
 from distutils.core import Extension
 
-setup(name = 'pyg-base', version = '0.0.112', packages = find_packages(), python_requires = '>=3.6.')
+setup(name = 'pyg-base', version = '0.0.113', packages = find_packages(), python_requires = '>=3.6.')
